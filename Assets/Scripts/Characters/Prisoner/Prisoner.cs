65bfa79ef4b6365a2621d270678ad4931f127bae
Anthony <transkuja@hotmail.fr>
2017-02-18 15:54:56 +0100

    Create Prisoner (class, instance and in scene). Add PrisonerInstance layer.

---
+++ Assets/Scripts/Characters/Prisoner/Prisoner.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Prisoner : Character {
+
+    [SerializeField]
+    private GameObject associatedSprite;
+
+    [Header("Status")]
+
+    [SerializeField]
+    private short actualHunger = 0;
+    [SerializeField]
+    [Range(50, 120)]
+    private short maxHunger = 100;
+
+    [SerializeField]
+    private short maxMentalHealth = 100;
+
+    [SerializeField]
+    private short actualMentalHealth;
+
+
+    private bool isStarving = false;
+    private bool isMentalHealthLow = false;
+
+    public short ActualHunger
+    {
+        get { return actualHunger; }
+        set
+        {
+            actualHunger = value;
+            if (actualHunger > MaxHunger)
+            {
+                actualHunger = MaxHunger;
+                isStarving = true;
+            }
+            else if (actualHunger < 0)
+            {
+                actualHunger = 0;
+                isStarving = false;
+            }
+            else
+            {
+                isStarving = false;
+            }
+
+        }
+    }
+
+    public short ActualMentalHealth
+    {
+        get { return actualMentalHealth; }
+        set
+        {
+            actualMentalHealth = value;
+            if (actualMentalHealth < 0)
+            {
+                actualMentalHealth = 0;
+                isMentalHealthLow = true;
+            }
+            else if (actualMentalHealth > maxMentalHealth)
+            {
+                actualMentalHealth = maxMentalHealth;
+                isMentalHealthLow = false;
+            }
+            else
+            {
+                isMentalHealthLow = false;
+            }
+        }
+    }
+
+    public GameObject AssociatedSprite
+    {
+        get
+        {
+            return associatedSprite;
+        }
+
+        set
+        {
+            associatedSprite = value;
+        }
+    }
+
+    public short MaxHunger
+    {
+        get
+        {
+            return maxHunger;
+        }
+
+        private set { }
+    }
+
+    public Prisoner()
+    {
+        actualMentalHealth = maxMentalHealth;
+    }
+
+    public Prisoner(Prisoner from)
+    {
+        associatedSprite = from.associatedSprite;
+    }
+}
---
+++ Assets/Scripts/Characters/Prisoner/PrisonerInstance.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrisonerInstance : MonoBehaviour {
+
+    [Header("Prisoner Info")]
+    [SerializeField]
+    private Prisoner prisoner = null;
+
+    public PrisonerInstance(PrisonerInstance from)
+    {
+        prisoner = from.prisoner;
+    }
+
+    /* ------------------------------------------------------------------------------------ */
+    /* ------------------------------------- Accessors ------------------------------------ */
+    /* ------------------------------------------------------------------------------------ */
+    public Prisoner Prisoner
+    {
+        get
+        {
+            return prisoner;
+        }
+
+        set
+        {
+            prisoner = value;
+        }
+    }
+}
