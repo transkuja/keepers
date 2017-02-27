ee0c3d630ebb4ec7dd629f65bbfee211af3c6b1d
Unknown <quentin.peuchmaur@gmail.com>
2017-02-27 17:52:14 +0100

    Condiiton de victoire

---
+++ Assets/Scripts/Board/Arrival.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrival : MonoBehaviour {
+
+    InteractionImplementer interationImplementer;
+
+    [SerializeField]
+    private Tile tileArrival;
+
+    // Use this for initialization
+    void Start () {
+        interationImplementer = new InteractionImplementer();
+        interationImplementer.Add(new Interaction(EndGame), "End Game", null);  
+	}
+
+    public void EndGame(int i = -1)
+    {
+        if(TileManager.Instance.PrisonerTile == tileArrival)
+        {
+            Debug.Log("You win");
+        }
+        else
+        {
+            Debug.Log("Nope");
+        }
+
+    }
+
+    public InteractionImplementer InterationImplementer
+    {
+        get
+        {
+            return interationImplementer;
+        }
+    }
+}
