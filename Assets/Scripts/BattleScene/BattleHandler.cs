ce5ac7d52cecf90c129b8027978ec85282aa068e
Anthony <transkuja@hotmail.fr>
2017-03-11 20:15:33 +0100

    Change mocked resolution to computed resolution

--- Assets/Scripts/BattleScene/BattleHandler.cs
+++ Assets/Scripts/BattleScene/BattleHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BattleHandler {
-
+    private enum AttackType { Physical, Magical }
     // Is the prisoner on the tile where the battle is processed
     public static bool isPrisonerOnTile = false;
 
@@ -47,6 +47,8 @@ public class BattleHandler {
         {
             HandleBattleDefeat(selectedKeepersForBattle);
         }
+
+        PostBattleCommonProcess(selectedKeepersForBattle, tile);
     }
 
     /*
@@ -54,22 +56,180 @@ public class BattleHandler {
      */
     private static bool ResolveBattle(List<KeeperInstance> keepers, Tile tile)
     {
-        List<MonsterInstance> monsters = TileManager.Instance.MonstersOnTile[tile];
+        List<MonsterInstance> monsters = new List<MonsterInstance>();
+        monsters.AddRange(TileManager.Instance.MonstersOnTile[tile]);
 
         // General melee!
+        int totalDamageTaken = 0;
 
-        // Mock
+        while (monsters.Count > 0 && totalDamageTaken < 50)
+        {
+            int totalTurns = keepers.Count + monsters.Count;
+            int turnCounter = 0;
+            while (turnCounter < totalTurns)
+            {
+                if ((turnCounter + 1) % 2 == 1
+                    && (turnCounter + 1) <= (keepers.Count * 2) - 1)
+                {
+                    // Keeper turn
+                    KeeperInstance attacker = keepers[turnCounter / 2];
+                    MonsterInstance target;
+                    AttackType attackType = attacker.Keeper.GetEffectiveStrength() > attacker.Keeper.GetEffectiveIntelligence() ? AttackType.Physical : AttackType.Magical;
 
-        if (keepers[0].Keeper.CharacterName == "MockBattle")
+                    target = GetTargetForAttack(monsters, attackType);
+                    // Inflict damage to target
+                    KeeperDamageCalculation(attacker, target, attackType);
+
+                    // Remove monster from list if dead
+                    if (target.CurrentHp <= 0)
+                    {
+                        target.CurrentHp = 0;
+                        monsters.Remove(target);
+                    }
+
+                    if (monsters.Count == 0)
+                        break;
+                }
+                else
+                {
+                    // Monster turn
+                    MonsterInstance attacker = monsters[turnCounter <= keepers.Count * 2 ? turnCounter / 2 : turnCounter - keepers.Count];
+                    bool isPrisonerTargeted = false;
+                    AttackType attackType = attacker.Monster.GetEffectiveStrength() > attacker.Monster.GetEffectiveIntelligence() ? AttackType.Physical : AttackType.Magical;
+
+                    if (isPrisonerOnTile)
+                    {
+                        float determineTarget = Random.Range(0, 100);
+                        if (determineTarget < (100 / keepers.Count + 2) * 2)
+                            isPrisonerTargeted = true;
+                    }
+
+                    if (isPrisonerTargeted)
+                    {
+                        totalDamageTaken += MonsterDamageCalculation(attacker, null, attackType, true);
+                    }
+                    else
+                    {
+                        KeeperInstance target = GetTargetForAttack(keepers);
+                        totalDamageTaken += MonsterDamageCalculation(attacker, target, attackType);
+
+                        if (target.CurrentHp <= 0)
+                        {
+                            target.CurrentHp = 0;
+                            keepers.Remove(target);
+                        }
+                    }
+
+                    if (totalDamageTaken >= 50)
+                        break;
+                    else if (GameManager.Instance.PrisonerInstance.CurrentHp <= 0)
+                        break;
+                }
+
+                turnCounter++;
+            }
+        }
+
+        // Battle result
+        if (monsters.Count == 0)
+        {
+            Debug.Log("Battle won! Yippi!");
+            return true;
+        }
+        else
         {
             Debug.Log("Battle lost");
             return false;
         }
-
-        Debug.Log("Battle won! Yippi!");
-        return true;
     }
 
+    private static MonsterInstance GetTargetForAttack(List<MonsterInstance> monsters, AttackType attackType)
+    {
+        List<MonsterInstance> subMonstersList = new List<MonsterInstance>();
+        foreach (MonsterInstance mi in monsters)
+        {
+            if (mi.CurrentHp == 0)
+            {
+                continue;
+            }
+
+            if (attackType == AttackType.Physical)
+            {
+                if (mi.Monster.GetEffectiveSpirit() > mi.Monster.GetEffectiveDefense())
+                    subMonstersList.Add(mi);
+            }
+            else
+            {
+                if (mi.Monster.GetEffectiveSpirit() < mi.Monster.GetEffectiveDefense())
+                    subMonstersList.Add(mi);
+            }
+        }
+
+        if (subMonstersList.Count == 0)
+        {
+            subMonstersList.AddRange(monsters);
+        }
+
+        MonsterInstance target = null;
+        int tmpHp = 100;
+
+        foreach (MonsterInstance mi in subMonstersList)
+        {
+            if (mi.CurrentHp <= tmpHp)
+            {
+                target = mi;
+                tmpHp = mi.CurrentHp;
+            }
+        }
+
+        return target;
+    }
+
+    private static KeeperInstance GetTargetForAttack(List<KeeperInstance> keepers)
+    {
+        return keepers[Random.Range(0, keepers.Count)];
+    }
+
+    private static void KeeperDamageCalculation(KeeperInstance attacker, MonsterInstance targetMonster, AttackType attackType)
+    {
+        int damage = 0;
+        if (attackType == AttackType.Physical)
+        {
+            damage = Mathf.RoundToInt(Mathf.Pow(attacker.Keeper.GetEffectiveStrength(), 2) / targetMonster.Monster.GetEffectiveDefense());
+        }
+        else
+        {
+            damage = Mathf.RoundToInt(Mathf.Pow(attacker.Keeper.GetEffectiveIntelligence(), 2) / targetMonster.Monster.GetEffectiveSpirit());
+        }
+
+        targetMonster.CurrentHp -= damage;
+    }
+
+    private static int MonsterDamageCalculation(MonsterInstance attacker, KeeperInstance targetKeeper, AttackType attackType, bool prisonerTargeted = false)
+    {
+        int damage = 0;
+        if (attackType == AttackType.Physical)
+        {
+            if (prisonerTargeted)
+                damage = Mathf.RoundToInt(attacker.Monster.GetEffectiveStrength() / targetKeeper.Keeper.GetEffectiveDefense());
+            else
+                damage = Mathf.RoundToInt(attacker.Monster.GetEffectiveStrength() / GameManager.Instance.PrisonerInstance.Prisoner.GetEffectiveDefense());
+        }
+        else
+        {
+            if (prisonerTargeted)
+                damage = Mathf.RoundToInt(attacker.Monster.GetEffectiveIntelligence() / targetKeeper.Keeper.GetEffectiveSpirit());
+            else
+                damage = Mathf.RoundToInt(attacker.Monster.GetEffectiveIntelligence() / GameManager.Instance.PrisonerInstance.Prisoner.GetEffectiveSpirit());
+        }
+
+        if (prisonerTargeted)
+            GameManager.Instance.PrisonerInstance.CurrentHp -= damage;
+        else
+            targetKeeper.CurrentHp -= damage;
+
+        return damage;
+    }
 
     /*
      * Process everything that needs to be processed after a victory
@@ -89,12 +249,6 @@ public class BattleHandler {
         }
 
         PrintVictoryScreen();
-        Item[] loot = ComputeTotalLoot(tile);
-        TileManager.Instance.RemoveDefeatedMonsters(tile);
-
-        // TODO: @Remi Open loot window
-        IngameScreens.Instance.goInventoryLoot.GetComponentInParent<Inventory>().inventory = loot;
-        IngameScreens.Instance.UpdateLootInterface();
     }
 
     /*
@@ -123,6 +277,25 @@ public class BattleHandler {
         }
 
         PrintDefeatScreen(10, 5, 10);
+
+    }
+
+    private static void PostBattleCommonProcess(List<KeeperInstance> keepers, Tile tile)
+    {
+        Item[] loot = ComputeTotalLoot(tile);
+        Debug.Log(loot);
+        for (int i = 0; i < loot.Length; i++)
+            Debug.Log(loot[i].sprite);
+        TileManager.Instance.RemoveDefeatedMonsters(tile);
+
+        Debug.Log(loot);
+        for (int i = 0; i < loot.Length; i++)
+            Debug.Log(loot[i].sprite);
+        if (loot != null && loot.Length > 0)
+        {
+            IngameScreens.Instance.goInventoryLoot.GetComponentInParent<Inventory>().inventory = loot;
+            IngameScreens.Instance.UpdateLootInterface();
+        }
     }
 
     private static void PrintVictoryScreen()
@@ -161,7 +334,8 @@ public class BattleHandler {
         List<Item> lootList = new List<Item>();
         foreach (MonsterInstance mi in TileManager.Instance.MonstersOnTile[tile])
         {
-            lootList.AddRange(mi.GetComponent<Loot>().ComputeLoot());
+            if (mi.CurrentHp == 0)
+                lootList.AddRange(mi.GetComponent<Loot>().ComputeLoot());
         }
 
         return lootList.ToArray();
