950d12d5c207a995ef1f03d00a8df5344840886f
Seb <Sebdami@hotmail.fr>
2017-03-20 17:14:42 +0100

    Fix and Update Greyed Tiles

---
+++ Assets/Scripts/Board/Tiles/GreyedTileModel.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreyedTileModel : MonoBehaviour {
+    //Used to know if a greyed model has already been instanciated
+}
---
+++ Assets/Scripts/Board/Tiles/TileAspectChanger.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TileAspectChanger : MonoBehaviour {
+    
+    [SerializeField]
+    GameObject greyedPrefab;
+    GreyedTileModel tileModel = null;
+    Tile tile;
+
+    public void SetAsGreyedModel()
+    {
+        
+        tileModel.gameObject.SetActive(true);
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    public void SetAsBaseModel()
+    {
+        tileModel.gameObject.SetActive(false);
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    void UpdateModel()
+    {
+        if (tile.State == TileState.Greyed)
+            SetAsGreyedModel();
+        else if (tile.State == TileState.Discovered)
+            SetAsBaseModel();
+    }
+
+    // Use this for initialization
+    void Start () {
+        tile = GetComponentInParent<Tile>();
+        tileModel = GetComponentInChildren<GreyedTileModel>();
+        if (tileModel == null)
+        {
+            GameObject go = Instantiate(greyedPrefab, transform);
+            go.transform.SetAsLastSibling();
+            go.transform.position = transform.position;
+            if (GetComponentInChildren<GreyedTileModel>() == null)
+                go.AddComponent<GreyedTileModel>();
+            tileModel = go.GetComponent<GreyedTileModel>();
+            tileModel.gameObject.SetActive(false);
+        }
+        
+
+        UpdateModel();
+
+        tile.StateChanged += UpdateModel;
+	}
+	
+    void OnDestroy()
+    {
+            //tile.StateChanged -= UpdateModel;
+    }
+}
