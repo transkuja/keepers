71bd3d3d126344d9c99cbbba518676325e8e375b
Anthony <transkuja@hotmail.fr>
2017-03-21 12:23:43 +0100

    Update wolf so it can now aggro keepers

---
+++ Assets/Scripts/Characters/Monsters/AggroBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AggroBehaviour : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<KeeperInstance>() != null)
+        {
+            GetComponentInParent<NavMeshAgent>().ResetPath();
+            GetComponentInParent<NavMeshAgent>().SetDestination(other.transform.position);
+        }
+    }
+}
