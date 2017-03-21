4dee913c3b31a904b74dc9441ed0d72810ac316c
Unknown <remi.boscher@gmail.com>
2017-03-21 16:33:30 +0100

    Selection Feedback grossi de loin

---
+++ Assets/BillboardForPointer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardForPointer : MonoBehaviour {
+
+    public void RecalculateActionCanvas()
+    {
+        transform.localScale = new Vector3(1,1,1) + new Vector3(1,1,1) * 2 * (1 - GameManager.Instance.CameraManager.FZoomLerp);
+    }
+
+    void Update()
+    {
+        RecalculateActionCanvas();
+    }
+}
