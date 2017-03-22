8d895e4e40f5e81ee55eecca9f72aa034e01907b
Unknown <remi.boscher@gmail.com>
2017-03-22 13:19:40 +0100

    God's work

--- Assets/Scripts/BoardScene/UI/EndTurn/AnimationButtonClick.cs
+++ Assets/Scripts/BoardScene/UI/EndTurn/AnimationButtonClick.cs
@@ -7,6 +7,52 @@ using UnityEngine.UI;
 
 public class AnimationButtonClick : MonoBehaviour, IPointerEnterHandler {
 
+    private Light directionalLight;
+    private float temps;
+
+    public void Start()
+    {
+        directionalLight = GameObject.Find("Directional Light").GetComponent<Light>();
+
+        AnimationClip ac = null;
+        for (int i = 0; i < GetComponent<Animator>().runtimeAnimatorController.animationClips.Length; i++)
+        {
+            if (GetComponent<Animator>().runtimeAnimatorController.animationClips[i].name == "NewTurn")
+            {
+                ac = GetComponent<Animator>().runtimeAnimatorController.animationClips[i];
+                break;
+            }
+      
+        }
+        temps = ac.length;
+    }
+
+
+    public void ChangeLight()
+    {
+        StartCoroutine(GodsWork());
+    }
+
+    private IEnumerator GodsWork()
+    {
+        directionalLight.intensity = Mathf.Clamp(directionalLight.intensity, 0, 1);
+        for (float f = temps/2; f >= 0; f -= Time.deltaTime)
+        {
+            //valeur = 0
+            directionalLight.intensity -= Time.deltaTime *2;
+
+
+            yield return null;
+        }
+
+        for (float f = temps / 2; f >= 0; f -= Time.deltaTime)
+        {
+            // valeur = 1
+            directionalLight.intensity += Time.deltaTime*2;
+            yield return null;
+        }
+    }
+
     // Update is called once per frame
     public void HandleAnimation() {
         GameManager.Instance.Ui.TurnPanel.transform.GetChild(0).GetComponentInChildren<Text>().text = "Jour " + ++GameManager.Instance.NbTurn;
@@ -40,5 +86,4 @@ public class AnimationButtonClick : MonoBehaviour, IPointerEnterHandler {
         GameManager.Instance.Ui.goShortcutKeepersPanel.transform.GetChild(_i + 1).GetChild(4).transform.localScale = Vector3.one;
         yield return null;
     }
-
 }
