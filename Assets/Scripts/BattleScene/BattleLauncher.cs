343098b7cc8cc331906879887758d129267b3a82
Anthony <transkuja@hotmail.fr>
2017-03-24 19:28:02 +0100

    Fix battle launcher so battle can start when monsters hit the prisoner

--- Assets/Scripts/BattleScene/BattleLauncher.cs
+++ Assets/Scripts/BattleScene/BattleLauncher.cs
@@ -9,7 +9,11 @@ public class BattleLauncher : MonoBehaviour {
     {
         if (other.gameObject.GetComponent<MonsterInstance>())
         {
-            Tile tile = TileManager.Instance.GetTileFromKeeper[GetComponentInParent<KeeperInstance>()];
+            Tile tile;
+            if (GetComponentInParent<KeeperInstance>() != null)
+                tile = TileManager.Instance.GetTileFromKeeper[GetComponentInParent<KeeperInstance>()];
+            else
+                tile = TileManager.Instance.PrisonerTile;
 
             GetComponentInParent<NavMeshAgent>().SetDestination(transform.position);
             foreach (MonsterInstance mi in TileManager.Instance.MonstersOnTile[tile])
