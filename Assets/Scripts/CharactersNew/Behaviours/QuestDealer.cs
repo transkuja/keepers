7f5f37cc14069bdce878fa0ae1380d66681b0180
Anthony <transkuja@hotmail.fr>
2017-03-29 11:08:38 +0200

    Add Quest Dealer to behaviours

---
+++ Assets/Scripts/CharactersNew/Behaviours/QuestDealer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestDealer : MonoBehaviour {
+
+    public GameObject prefabContentQuest;
+    public GameObject goQuest;
+
+    void Awake()
+    {
+        goQuest = Instantiate(prefabContentQuest, GameManager.Instance.Ui.baseQuestPanel.transform);
+        goQuest.transform.localPosition = Vector3.zero;
+        goQuest.transform.localScale = Vector3.one;
+    }
+}
