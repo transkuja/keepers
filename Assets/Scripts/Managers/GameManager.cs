49208a9c3017b8b8953147913e9a38cfda34b1b8
Seb <Sebdami@hotmail.fr>
2017-03-30 17:19:37 +0200

    Add very important comment just so I remember

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -116,6 +116,7 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        // I NEED A QUEST INITILIZER
         List<IQuestObjective> mainObjectives = new List<IQuestObjective>();
         mainObjectives.Add(new PrisonerEscortObjective("Until the end", "Bring Ashley to the end, and ALIVE.", GameObject.FindObjectOfType<Behaviour.Prisoner>().gameObject, TileManager.Instance.EndTile.GetComponent<Tile>()));
         mainQuest = new Quest(new QuestIdentifier(0, gameObject), new QuestText("Main Quest: The last phoque licorne", "", "You're probably wondering why I gathered all of you here today. Well I'll be quick, I want you to bring this wonderful animal to my good and rich friend. Don't worry, you will be rewarded. His name is \"End\", you'll see his flag from pretty far away, head towards it. I'm counting on you, it is extremely important.", "Hint: Don't kill Ashley."), mainObjectives);
