305d0b92d202ac3e09722d920d0b3eabd60f33ed
Rémi <remi.boscher@gmail.com>
2017-04-19 18:45:06 +0200

    Anais elle file des trucs et sa marche pas

--- Assets/TutoManager.cs
+++ Assets/TutoManager.cs
@@ -2,25 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class TutoManager : MonoBehaviour {
 
     private static TutoManager s_instance = null;
 
     public bool enableTuto = false;
-    private SequenceTuto playingSequence;
-
     private List<SequenceTuto> sequences;
 
+    public enum SequenceState { Idle, WaitingForInput, End };
 
-    public IEnumerator MyFunction(GameObject go, float delayTime)
-    {
-        yield return new WaitForSeconds(delayTime);
-        go.SetActive(true);
-        // Now do your thing here
-    }
-
-
+    private SequenceTuto playingSequence;
     public SequenceTuto PlayingSequence
     {
         get
@@ -35,26 +29,25 @@ public class TutoManager : MonoBehaviour {
         }
     }
 
-    public abstract class SequenceTuto
-    {
-        private bool alreadyPlayed = false;
-
-        public abstract void Sequence();
-        public abstract void EndStateSequence();
-    }
-
     public void Update()
     {
         if (playingSequence != null)
         {
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Escape))
+            if ((Input.GetKeyDown(KeyCode.Space)|| Input.GetMouseButtonDown(0)) && playingSequence.currentState == SequenceState.WaitingForInput)
+            {
+                playingSequence.currentState = SequenceState.Idle;
+                s_instance.playNextSequence(playingSequence);
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape))
             {
                 playingSequence.EndStateSequence();
+            }           
+            else if ( playingSequence.currentState == SequenceState.End)
+            {
+                PlayingSequence = null;
             }
         }
     }
-
-
 
     // Use this for initialization
     void Start () {
@@ -65,38 +58,117 @@ public class TutoManager : MonoBehaviour {
             sequences.Add(new SequenceIntro());
             playSequence(sequences[0]);
         }
+    }
 
+    public void playSequence(SequenceTuto st)
+    {
+        s_instance.PlayingSequence = st;
+        st.Sequence();
+    }
 
+    public void playNextSequence(SequenceTuto st)
+    {
+        if(st.currentState != SequenceState.End)
+        StartCoroutine(st.listSequences[st.nextSeq]);
+    }
+
+    #region Sequences
+    public abstract class SequenceTuto
+    {
+        public bool alreadyPlayed = false;
+        public SequenceState currentState;
+        public List<IEnumerator> listSequences;
+        public int nextSeq;
+
+        public abstract void Sequence();
+        public abstract void EndStateSequence();
     }
 
     public class SequenceIntro : SequenceTuto
     {
+        public GameObject goMrResetti;
+
         public override void Sequence()
         {
-            GameObject go = GameManager.Instance.PawnDataBase.CreatePawn("ashley", new Vector3(0.0f, 0.0f, -1.0f), Quaternion.identity, null);
-            go.SetActive(false);
-            if (go.GetComponent<Behaviour.Mortal>().DeathParticles != null)
+            currentState = SequenceState.Idle;
+            goMrResetti = SpawnMrResetti();
+            listSequences = new List<IEnumerator>();
+            listSequences.Add(Spawn(goMrResetti, 0.5f));
+            listSequences.Add(Intro(goMrResetti, 0.5f));
+            listSequences.Add(EndSeq(0.5f));
+
+            nextSeq = 0;
+            s_instance.playNextSequence(this);
+        }
+
+        public GameObject SpawnMrResetti()
+        {
+            goMrResetti = GameManager.Instance.PawnDataBase.CreatePawn("mrresetti", new Vector3(0.0f, 0.0f, -1.0f), Quaternion.identity, null);
+            goMrResetti.SetActive(false);
+            if (goMrResetti.GetComponent<Behaviour.Mortal>().DeathParticles != null)
             {
-                ParticleSystem ps = Instantiate(go.GetComponent<Behaviour.Mortal>().DeathParticles, go.transform.parent);
-                ps.transform.position = go.transform.position;
+                ParticleSystem ps = Instantiate(goMrResetti.GetComponent<Behaviour.Mortal>().DeathParticles, goMrResetti.transform.parent);
+                ps.transform.position = goMrResetti.transform.position;
                 ps.Play();
                 Destroy(ps.gameObject, ps.main.duration);
             }
-            s_instance.StartCoroutine(s_instance.MyFunction(go, 2.0f));
+            return goMrResetti;
+        }
 
+        public IEnumerator Spawn(GameObject mrresetti, float delayTime)
+        {
+            yield return new WaitForSeconds(delayTime);
+            mrresetti.SetActive(true);
+            mrresetti.GetComponentInChildren<Animator>().SetTrigger("jumpArround");
+
+
+            // TODO ? 
+            yield return new WaitForSeconds(mrresetti.GetComponentInChildren<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.length);
+
+            string str = "Salut c'est moi Mr Resetti. Bienvenue dans le tutoriel de \"Keepers\" !";
+            yield return EcrireMessage(mrresetti.GetComponent<Interactable>().Feedback, str);
+            currentState = SequenceState.WaitingForInput;
+            nextSeq = 1;
+        }
+        public IEnumerator Intro(GameObject mrresetti, float delayTime)
+        {
+            string str2 = "Le jour se lève sur la planète France, encore !";
+            yield return EcrireMessage(mrresetti.GetComponent<Interactable>().Feedback, str2);
+            currentState = SequenceState.WaitingForInput;
+            nextSeq = 2;
+        }
+
+        public IEnumerator EndSeq(float delayTime)
+        {
+            yield return new WaitForSeconds(delayTime);
+            if (goMrResetti != null) goMrResetti.SetActive(false);
+            currentState = SequenceState.End;
         }
 
 
         public override void EndStateSequence()
         {
             s_instance.PlayingSequence = null;
+            if (goMrResetti != null) goMrResetti.SetActive(false);
+            alreadyPlayed = true;
         }
     }
+    #endregion
 
-  
-    public void playSequence(SequenceTuto st)
+    public static IEnumerator EcrireMessage(Transform feedback,string str)
     {
-        s_instance.PlayingSequence = st;
-        st.Sequence();
+        feedback.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        feedback.GetComponentInChildren<Text>().text = string.Empty;
+        feedback.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        // Fête du sleep
+        for (int i = 0; i < str.Length; i++)
+        {
+            feedback.GetComponentInChildren<Text>().text += str[i];
+            yield return new WaitForSeconds(0.05f);
+        }
+        yield return new WaitForSeconds(0.5f);
+       
     }
 }
