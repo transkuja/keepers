7c78d467edabe8f2a950e26315637e6569cda62a
Anthony <transkuja@hotmail.fr>
2017-04-20 17:58:47 +0200

    Fix dice prefab

--- Assets/Scripts/BattleScene/DieBuilder.cs
+++ Assets/Scripts/BattleScene/DieBuilder.cs
@@ -5,12 +5,18 @@ public class DieBuilder : MonoBehaviour {
 	public static GameObject BuildDie(Die dieToBuild, Tile tileConcerned, Vector3 position)
     {
         GameObject dieInstance = Instantiate(GameManager.Instance.PrefabUtils.die, tileConcerned.transform);
+        float emissionColor = 0.75f;
+        Transform currentFace = dieInstance.transform.GetChild(0);
+        Material currentFaceMaterial = currentFace.GetComponent<Renderer>().material;
+        currentFaceMaterial.mainTexture = GameManager.Instance.Texture2DUtils.attackX1;
+        currentFaceMaterial.SetTexture("_EmissionMap", GameManager.Instance.Texture2DUtils.attackX1);
+        currentFaceMaterial.SetColor("_EmissionColor", new Color(emissionColor, emissionColor, emissionColor));
+        currentFaceMaterial.EnableKeyword("_EMISSION");
+
         for (int i = 1; i <= 6; i++)
         {
-            float emissionColor = 0.75f;
-
-            Transform currentFace = dieInstance.transform.GetChild(i);
-            Material currentFaceMaterial = currentFace.GetComponent<Renderer>().material;
+            currentFace = dieInstance.transform.GetChild(i);
+            currentFaceMaterial = currentFace.GetComponent<Renderer>().material;
             currentFaceMaterial.mainTexture = GameManager.Instance.Texture2DUtils.GetTextureFromFaceData(dieToBuild.Faces[i - 1]);
             currentFaceMaterial.SetTexture("_EmissionMap", GameManager.Instance.Texture2DUtils.GetTextureFromFaceData(dieToBuild.Faces[i - 1]));
             currentFaceMaterial.SetColor("_EmissionColor", new Color(emissionColor, emissionColor, emissionColor));
@@ -18,7 +24,9 @@ public class DieBuilder : MonoBehaviour {
 
             currentFace.GetComponent<FaceComponent>().FaceData = dieToBuild.Faces[i - 1];
         }
+
         
+
         dieInstance.transform.position = GameManager.Instance.ActiveTile.transform.position + position;
         return dieInstance;
     }
