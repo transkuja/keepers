eab57034ef8097477b159214c7edaba4aa62fe32
Rémi <remi.boscher@gmail.com>
2017-05-03 16:50:21 +0200

    Butterfly

--- Assets/VolePetitPapillon.cs
+++ Assets/VolePetitPapillon.cs
@@ -26,22 +26,30 @@ public class VolePetitPapillon : MonoBehaviour {
         SineDistanceX = Random.Range(0.5f, 0.7f);
         SineDistanceY = Random.Range(0.5f, 0.7f);
         SineDistanceZ = Random.Range(0.5f, 0.7f);
-        offsetX = Random.Range(-0.5f, 0.5f);
-        offsetY = Random.Range(-0.5f, 0.5f);
-        offsetZ = Random.Range(-0.5f, 0.5f);
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
         i += Time.deltaTime;
+
+        if (i > 50.0f)
+        {
+            i = 0;
+            offsetX = Random.Range(-0.5f, 0.5f);
+            offsetY = Random.Range(-0.5f, 0.5f);
+            offsetZ = Random.Range(-0.5f, 0.5f);
+        }
         Vector3 oldpos = transform.position;
         Vector3 targetPos;
+
         targetPos.x = startPos.x + Mathf.Sin(i * Mathf.PI * SineSpeedX / 180) * SineDistanceX + offsetX;
         targetPos.y = startPos.y + Mathf.Sin(i * Mathf.PI * SineSpeedY / 180) * SineDistanceY + offsetY;
         targetPos.z = startPos.z + Mathf.Sin(i * Mathf.PI * SineSpeedZ / 180) * SineDistanceZ + offsetZ;
 
         transform.position = targetPos;
         transform.LookAt(transform.position + (targetPos - oldpos).normalized);
+
     }
 }
