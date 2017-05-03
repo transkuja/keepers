af8949d30cce451cc941ea25c0178744af8e4c0a
Rémi <remi.boscher@gmail.com>
2017-05-03 16:40:56 +0200

    Vol Petit Papillon 2.0

--- Assets/VolePetitPapillon.cs
+++ Assets/VolePetitPapillon.cs
@@ -7,33 +7,41 @@ public class VolePetitPapillon : MonoBehaviour {
     float i = 0.0f;
     float SineSpeedX;
     float SineSpeedY;
+    float SineSpeedZ;
     float SineDistanceX;
     float SineDistanceY;
+    float SineDistanceZ;
     float offsetX;
     float offsetY;
+    float offsetZ;
     Vector3 startPos;
     // Use this for initialization
     void Start()
     {
         i = 0.0f;
         startPos = transform.position;
-        SineSpeedX = Random.Range(50.0f, 80.0f);
-        SineSpeedY = Random.Range(50.0f, 80.0f);
-        SineDistanceX = Random.Range(0.5f, 1.0f);
-        SineDistanceY = Random.Range(0.5f, 1.0f);
-        offsetX = Random.Range(-2.0f, 2.0f);
-        offsetY = Random.Range(-2.0f, 2.0f);
+        SineSpeedX = Random.Range(30.0f, 50.0f);
+        SineSpeedY = Random.Range(30.0f, 50.0f);
+        SineSpeedZ = Random.Range(30.0f, 50.0f);
+        SineDistanceX = Random.Range(0.5f, 0.7f);
+        SineDistanceY = Random.Range(0.5f, 0.7f);
+        SineDistanceZ = Random.Range(0.5f, 0.7f);
+        offsetX = Random.Range(-0.5f, 0.5f);
+        offsetY = Random.Range(-0.5f, 0.5f);
+        offsetZ = Random.Range(-0.5f, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
         i += Time.deltaTime;
+        Vector3 oldpos = transform.position;
         Vector3 targetPos;
         targetPos.x = startPos.x + Mathf.Sin(i * Mathf.PI * SineSpeedX / 180) * SineDistanceX + offsetX;
         targetPos.y = startPos.y + Mathf.Sin(i * Mathf.PI * SineSpeedY / 180) * SineDistanceY + offsetY;
-        targetPos.z = startPos.z;
+        targetPos.z = startPos.z + Mathf.Sin(i * Mathf.PI * SineSpeedZ / 180) * SineDistanceZ + offsetZ;
 
         transform.position = targetPos;
+        transform.LookAt(transform.position + (targetPos - oldpos).normalized);
     }
 }
