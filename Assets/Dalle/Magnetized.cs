fc7f5bc5c943fd21441681e39f81947378a6e428
Rémi <remi.boscher@gmail.com>
2017-05-05 12:22:39 +0200

    Trigger For Quest

---
+++ Assets/Dalle/Magnetized.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Magnetized : MonoBehaviour {
+
+    /* Attraction drag must be set so the magnetized object will stop the closer from the target object. */
+    [SerializeField]
+    private short attractionDrag = 6;
+
+    /* Magnetized object speed. */
+    [SerializeField]
+    private float magnetizedVelocity = 1.0f;
+
+    private Vector3 storedrotation;
+
+    private float storedmass;
+
+
+    /* Magnetized object rigidbody. */
+    private Rigidbody magnetizedRigidbody;
+
+    void Start()
+    {
+        magnetizedRigidbody = GetComponent<Rigidbody>();
+        magnetizedRigidbody.useGravity = true;
+        magnetizedVelocity *= 50;
+		storedmass = magnetizedRigidbody.mass;
+		storedrotation = magnetizedRigidbody.transform.localEulerAngles;
+    }
+    
+    void onMouseDown () {
+
+		magnetizedRigidbody.useGravity = false;
+		magnetizedRigidbody.drag = attractionDrag;
+        // had to add this for a heavy object.
+        magnetizedRigidbody.mass = 1;
+
+        magnetizedRigidbody.AddForce (AttractForce (transform) * Time.deltaTime * magnetizedVelocity);
+		magnetizedRigidbody.transform.localEulerAngles= storedrotation;
+        
+
+    }
+
+    /* Computes the attract force vector. The vector points to target. */
+    private Vector3 AttractForce(Transform target)
+    {
+        Vector3 force = Vector3.zero;
+        force.x = target.position.x - transform.position.x;
+        force.y = target.position.y - transform.position.y;
+        //force.z = target.position.z - transform.position.z;
+        return force;
+    }
+
+}
---
+++ Assets/Dalle/TriggerOnCristals.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using Behaviour;
+
+public class TriggerOnCristals : MonoBehaviour {
+    private bool triggerActif;
+
+    public TriggerOnCristals monCopain;
+
+    private Material mat;
+    private Color storedColor;
+
+    public bool TriggerActif
+    {
+        get
+        {
+            return triggerActif;
+        }
+
+        set
+        {
+            triggerActif = value;
+            if(triggerActif == true)
+            {
+                if (monCopain.TriggerActif)
+                {
+                    GetComponent<PathBlocker>().enabled = false;
+                    monCopain.GetComponent<PathBlocker>().enabled = false;
+                }
+            } 
+   
+        }
+    }
+
+    public void Start()
+    {
+        mat = this.GetComponentInChildren<MeshRenderer>().material;
+		triggerActif = false;
+
+		storedColor = mat.GetColor ("_EmissionColor");
+    }
+		
+	// Quand le papillon ou le double se trouve a l'interieur
+	public void OnTriggerEnter(Collider col){
+        if (col.GetComponentInParent<Keeper>() != null && col.isTrigger)
+        {
+            TriggerActif = true;
+		}
+	}
+
+    public void OnTriggerExit(Collider col)
+    {
+        if (col.GetComponentInParent<Keeper>() != null && col.isTrigger)
+        {
+            TriggerActif = false;
+        }
+    }
+
+    public void Update()
+    {
+		if (!triggerActif) {
+			mat.SetColor ("_EmissionColor", storedColor);
+		} else {
+			mat.SetColor ("_EmissionColor", Color.blue);
+		}
+
+        transform.Rotate(Vector3.up, 0.5f);
+    }
+
+}
