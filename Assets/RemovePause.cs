5a4fc664b575f5678cc4f404b049f7b74899557c
Rémi <remi.boscher@gmail.com>
2017-05-05 14:49:06 +0200

    Correction

---
+++ Assets/RemovePause.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemovePause : MonoBehaviour {
+
+
+    public void OnDisable()
+    {
+        if (GameManager.Instance.CurrentState != GameState.InTuto || GameManager.Instance.CurrentState != GameState.InBattle)
+            GameManager.Instance.CurrentState = GameState.Normal;
+    }
+}
