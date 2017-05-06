d57cabb9e525bfc53ab061e306f9f73026e53710
Anthony <transkuja@hotmail.fr>
2017-05-06 20:30:16 +0200

    Revert buff feedback changes

--- Assets/Scripts/CharactersNew/Behaviours/Fighter.cs
+++ Assets/Scripts/CharactersNew/Behaviours/Fighter.cs
@@ -464,17 +464,6 @@ namespace Behaviour
                     effectiveBoeufs.Remove(effectiveBoeufs[i]);
                 }
             }
-            BuffFeedback bf = GetComponentInChildren<BuffFeedback>();
-            if (bf != null)
-                bf.UpdateCurrentBoeufsList(effectiveBoeufs);
-        }
-
-        public void AddBoeuf(BattleBoeuf _newBoeuf)
-        {
-            effectiveBoeufs.Add(_newBoeuf);
-            BuffFeedback bf = GetComponentInChildren<BuffFeedback>();
-            if (bf != null)
-                bf.UpdateCurrentBoeufsList(effectiveBoeufs);
         }
 
         // TODO: externalize this in Monster
