e9922299e3193014d9508559fd54d9c9a708b350
Anthony <transkuja@hotmail.fr>
2017-05-18 11:07:53 +0200

    Fix null reference exception when dropping in ashley slot from tuto scene

--- Assets/Scripts/CharactersNew/Behaviours/Inventory.cs
+++ Assets/Scripts/CharactersNew/Behaviours/Inventory.cs
@@ -341,7 +341,7 @@ namespace Behaviour
             if (GetComponent<Prisoner>() != null)
             {
                 Button feedButt = GetComponent<Inventory>().SelectedInventoryPanel.GetComponentInChildren<Button>();
-                feedButt.GetComponent<Image>().enabled = (GetComponent<HungerHandler>().CurrentHunger != GetComponent<HungerHandler>().Data.MaxHunger && !IsEmpty() && GetComponent<HungerHandler>().CurrentHunger != 0);
+                feedButt.GetComponent<Image>().enabled = (GetComponent<HungerHandler>() != null && GetComponent<HungerHandler>().CurrentHunger != GetComponent<HungerHandler>().Data.MaxHunger && !IsEmpty() && GetComponent<HungerHandler>().CurrentHunger != 0);
             }
 
         }
