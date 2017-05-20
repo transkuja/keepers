9f1147ae46634c44e5a318e60bc2cbcab39b0a51
Anthony <transkuja@hotmail.fr>
2017-05-20 22:51:26 +0200

    Tu m'as menti Seb

--- Assets/Scripts/BattleAnimations/SpriteAnimBattleAnimation.cs
+++ Assets/Scripts/BattleAnimations/SpriteAnimBattleAnimation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 [Serializable]
 public class SpriteAnimBattleAnimation : MonoBehaviour, IBattleAnimation {
@@ -23,8 +24,13 @@ public class SpriteAnimBattleAnimation : MonoBehaviour, IBattleAnimation {
     {
         foreach(PawnInstance go in targets)
         {
-            GameObject g = Instantiate<GameObject>(spriteWithAnimator, go.transform);
-            Destroy(g, GetAnimationTime());
+            GameObject g = Instantiate(spriteWithAnimator);
+            Canvas canvas = go.GetComponentInChildren<Canvas>();
+            g.transform.SetParent(canvas.transform);
+            g.transform.localPosition = Vector3.zero - canvas.transform.parent.transform.position.y/2.0f * Vector3.up;
+            g.transform.localScale = Vector3.one;
+            duration = g.GetComponent<SpriteAnimator>().GetAnimationTime();
+            g.GetComponent<SpriteAnimator>().Play();
         }
         
     }
---
+++ Assets/SpriteAnimator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteAnimator : MonoBehaviour {
+
+    [SerializeField]
+    Sprite[] sprites;
+    [SerializeField]
+    float animationSpeed;
+
+    public float GetAnimationTime()
+    {
+        return sprites.Length * animationSpeed;
+    }
+
+    public void Play()
+    {
+        StartCoroutine(AnimPlay());
+    }
+
+    public IEnumerator AnimPlay()
+    {
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            GetComponent<Image>().sprite = sprites[i];
+            yield return new WaitForSeconds(animationSpeed);
+        }
+        Destroy(gameObject);
+    }
+}
