fe614aaef5d4a8d8b977c181ead37e90834e4768
Seb <Sebdami@hotmail.fr>
2017-05-26 15:38:52 +0200

    Add some feedbacks

---
+++ Assets/FadeAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeAnim : MonoBehaviour {
+    [SerializeField]
+    AnimationCurve opacity;
+    [SerializeField]
+    float animationDuration = 1.0f;
+    Image sprite;
+    float timer = 0.0f;
+	void Start () {
+        timer = 0.0f;
+        sprite = GetComponent<Image>();	
+	}
+
+	void Update () {
+        timer += Time.deltaTime / (animationDuration+0.00001f); //Prevent dividing by zero
+        sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, opacity.Evaluate(timer));
+	}
+}
